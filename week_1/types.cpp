40f0d00e6570d1a7e04c9c5e7f1c89445f416283
Naraa1 <91043311+Naraa1@users.noreply.github.com>
2022-02-18 10:49:12 +0800

    Create types.cpp

---
+++ week_1/types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 1;
+    string b = "2";
+    double c = 0.3;
+    bool d = true;
+    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n";
+   
+    return 0;   
+}
